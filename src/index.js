ea836cf5a1afcb5553e8515fdc31fd08ee832a38
NikitaKozlov1997 <niki___tos@mail.ru>
2020-10-03 22:20:05 +0300

    fix: №1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length == 0) return 0;
+  let min = array[0];
+  for (let i=0; i<array.length; i++){
+    if ( array[i]<min ) {
+      min=array[i];
+  }
+} 
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length == 0) return 0;
+  let max=array[0];
+  for (let i=0; i<array.length; i++){
+      if ( array[i]> max ) {
+      max=array[i];
+    }
+  } 
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length == 0) return 0;
+  let cc =0;
+  let avg;
+  for (let i=0;i<array.length; i++){
+     cc+=array[i];
+  } 
+  avg= cc /array.length;
+  return avg;
 }
